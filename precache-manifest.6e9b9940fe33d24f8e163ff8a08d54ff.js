b3c857346a2585911692023466c498e58067dbee
Dmitriy Shirmanov <shirmanovdmitrtiy@gmail.com>
2019-02-23 20:59:51 +0700

    Updates

--- precache-manifest.6e9b9940fe33d24f8e163ff8a08d54ff.js
+++ precache-manifest.6e9b9940fe33d24f8e163ff8a08d54ff.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/coffeeShop/static/js/runtime~main.0a8da4d8.js"
   },
   {
-    "revision": "b4864ff83bf787650f79",
-    "url": "/coffeeShop/static/js/main.b4864ff8.chunk.js"
+    "revision": "5405473406acd830ba41",
+    "url": "/coffeeShop/static/js/main.54054734.chunk.js"
   },
   {
     "revision": "cd94d2ebeb9f70271fed",
     "url": "/coffeeShop/static/js/2.cd94d2eb.chunk.js"
   },
   {
-    "revision": "b4864ff83bf787650f79",
+    "revision": "5405473406acd830ba41",
     "url": "/coffeeShop/static/css/main.03071242.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/coffeeShop/static/css/2.94590af9.chunk.css"
   },
   {
-    "revision": "caeba9be173b122063c3e1414802080b",
+    "revision": "9f547358d915491bdcf912c1cce06ef0",
     "url": "/coffeeShop/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/coffeeShop/precache-manifest.2118fe87d19697da6b091dfc8ddc0989.js"
+  "/coffeeShop/precache-manifest.6e9b9940fe33d24f8e163ff8a08d54ff.js"
 );
 
 workbox.clientsClaim();
