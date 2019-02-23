afc95d8773d7d993e84fc0b4405f94c7f3bf06b6
Dmitriy Shirmanov <shirmanovdmitrtiy@gmail.com>
2019-02-23 20:31:34 +0700

    Updates

---
+++ precache-manifest.91e4aec2738d665a51b9585403e99042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a8da4d8378812bfa816",
+    "url": "/coffeeShop/static/js/runtime~main.0a8da4d8.js"
+  },
+  {
+    "revision": "f6e9e9cfd6c22489ac3d",
+    "url": "/coffeeShop/static/js/main.f6e9e9cf.chunk.js"
+  },
+  {
+    "revision": "38667130d2c64eb19ed3",
+    "url": "/coffeeShop/static/js/2.38667130.chunk.js"
+  },
+  {
+    "revision": "f6e9e9cfd6c22489ac3d",
+    "url": "/coffeeShop/static/css/main.f79a2fc2.chunk.css"
+  },
+  {
+    "revision": "38667130d2c64eb19ed3",
+    "url": "/coffeeShop/static/css/2.94590af9.chunk.css"
+  },
+  {
+    "revision": "9e3404708cc351f8f2367886593cb492",
+    "url": "/coffeeShop/index.html"
+  }
+];
